6ec09d026d94ec1dd7ab96e6f5fd338dca6e750b
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2025-08-10 23:33:10 +0530

    main serveer

---
+++ server/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import authRoutes from './routes/authRoutes.js';
+import tripRoutes from './routes/tripRoutes.js';
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/trips', tripRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+  console.log('MongoDB Connected: localhost');
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+})
+.catch((err) => console.error(err));
